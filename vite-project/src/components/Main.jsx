3cb6c56c8eb4914bf72458e0e38d8240f8ff67d2
saipunya <sumetchoorat@gmail.com>
2024-07-06 09:44:19 +0700

    Update Main.jsx

--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const Main = () => {
     const mainStyle = {
@@ -6,10 +6,26 @@ const Main = () => {
         width: '100%',
         maxWidth: 800,
         margin: '0 auto',
+        name1 : {
+            width: '100%',
+            height : 40,
+            marginTop : 10,
+            fontSize : 25,
+            outline : 'none',
+        }
     }
+    const showAlert = () => {
+        let name1 = document.getElementById('name1');
+        let s1 = document.getElementById('s1');
+        s1.innerHTML = name1.value;
+        name1.value = '';
+    };
     return (
         <div style={mainStyle}>
-              <img src="../img/opod.png" alt="" width="100%" />
+             <input type="text" name="" id="name1" style={mainStyle.name1} /><br></br>
+             <span id="s1" style={{color:'red',fontWeight:'bold'}}></span>
+             <div><button style={{marginTop:15}} onClick={showAlert}>OK button</button></div>
+              
         </div>
     );
 }
